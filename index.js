8e881c5c577f09cc9207be750e491e6650100fcf
karademirmustafa <71528859+karademirmustafa@users.noreply.github.com>
2024-10-01 20:37:26 +0300

    Kısa javascript ders notları

---
+++ index.js
@@ -0,0 +1,60 @@
+var btk=123;
+let btkakademi=123;
+const btktanimlama=123; // sabit verilen şey
+
+// console.log(btkakademi);
+
+
+// 2 tür fonksiyon var
+const arrow_fonksiyon = () => {
+
+}
+
+function normal_fonksiyon(){
+
+}
+
+const topla = (a,b) => {
+    return a+b;
+}
+
+console.log(topla(8,-8))
+
+console.log(topla(1,2))
+
+// veri tipleri
+//String 
+//Number(Float,Int)
+// Object
+// Array
+
+// console.log("String")
+// console.log(123) // INt
+// console.log(12.3) // Float
+// console.log({name:"Mustafa",surname:"Karademir"});
+// console.log(["Armut","Elma","Ayva"]);
+
+const sayi=91;
+
+// çift mi tek mi condition
+
+// if(sayi==92){
+//     console.log("Doğru sayıyı bildin");
+// }else if(sayi==94){
+//     console.log("91 sayısını yazdın");
+// }
+// else{
+//     console.log("Yanlış sayı",sayi);
+// }
+
+// Döngüler
+const yemek = "Lahmacun";
+// for, map, forEach
+// for(let i=0;i<yemek.length;i++){
+//     console.log(i,"İ değeri")
+// }
+const ulkeler = ["Türkiye","Azerbaycan","Filistin","İngiltere","Fransa"];
+
+// ulkeler.forEach((ulke,index) => {
+//     console.log(ulke,index,": Ülke")
+// })
