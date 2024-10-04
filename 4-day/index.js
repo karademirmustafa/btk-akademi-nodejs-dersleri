1f6c38ad6ca8a6764f3e5416ba0216de9063a6d9
karademirmustafa <71528859+karademirmustafa@users.noreply.github.com>
2024-10-04 19:23:18 +0300

    4. gün ilk aşamalar

---
+++ 4-day/index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+
+const app = express();
+const mongoose = require("mongoose");
+
+app.use(express.json());
+const MONGO_URI =
+  "mongodb+srv://karademirbayburt10:Zvr1YVcvrSSCvYJV@cluster0.gnbdk.mongodb.net/btk-akademi";
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+const OgrenciSchema = new mongoose.Schema({
+  adi: { type: String },
+  numarasi: { type: Number },
+  telefon_numarasi: { type: String },
+});
+const OgretmenSchema = new mongoose.Schema({
+  adi: { type: String },
+  alani: { type: String },
+  tel_no: { type: String },
+});
+
+const VeliSchema = new mongoose.Schema({
+  adi: { type: String },
+  ogrenci_adi: { type: String },
+});
+
+const Ogrenci = mongoose.model("Ogrenci", OgrenciSchema);
+const Ogretmen = mongoose.model("Ogretmen", OgretmenSchema);
+const Veli = mongoose.model("Veli", VeliSchema);
+
+app.post("/ogrenci", async (req, res, next) => {
+  const { adi, numarasi, telefon_numarasi } = req.body;
+
+  const ogrenci = await Ogrenci.create({
+    adi: adi,
+    numarasi,
+    telefon_numarasi,
+  });
+  return res.status(201).json(ogrenci);
+});
+app.get("/ogrenci", async (req, res, next) => {
+  const ogrenci = await Ogrenci.find();
+  return res.status(200).json(ogrenci);
+});
+app.get("/ogrenci/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const ogrenci = await Ogrenci.findById(id);
+  return res.status(200).json(ogrenci);
+});
+
+app.put("/ogrenci/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const {adi,numarasi,telefon_numarasi} = req.body
+
+  const ogrenci = await Ogrenci.findByIdAndUpdate(id,{
+    adi: adi,
+    numarasi,
+    telefon_numarasi,
+  },{new:true});
+  return res.status(200).json(ogrenci);
+});
+app.delete("/ogrenci/:id", async (req, res, next) => {
+  const { id } = req.params;
+
+  const ogrenci = await Ogrenci.findByIdAndDelete(id);
+  return res.status(200).json(ogrenci);
+});
+
+const PORT=8080
+app.listen(PORT, () => {
+  console.log(`Sunucu şu portta çalışıyor : ${PORT}`);
+});
