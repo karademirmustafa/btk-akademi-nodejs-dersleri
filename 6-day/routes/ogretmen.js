254e6f861ffa361dd69ef11d821078edaa4136b5
karademirmustafa <71528859+karademirmustafa@users.noreply.github.com>
2024-10-09 20:27:12 +0300

    validation eklendi

--- 6-day/routes/ogretmen.js
+++ 6-day/routes/ogretmen.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const  {fetchAllOgretmen,fetchByIdOgretmen,insertOgretmen,updateOgretmen,removeOgretmen} = require("../controllers/ogretmen");
-
+const {newOgretmenValidation} = require("../validations/ogretmen");
+const {validate} = require("../middlewares/validate");
 router.route('/').get(fetchAllOgretmen);
 router.route('/:ogretmen_id').get(fetchByIdOgretmen);
-router.route('/').post(insertOgretmen);
+router.route('/').post(validate(newOgretmenValidation),insertOgretmen);
 // router.route('/:id').put(updateOgretmen);
 // router.route('/:id').delete(removeOgretmen);
 
